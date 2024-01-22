88a2c80703c4b82a37c8a12675c0dfa3f403c986
Jerezard <jeremys-1999@hotmail.com>
2024-01-22 14:19:02 +0100

    added picture with transitions

--- script.js
+++ script.js
@@ -7,3 +7,11 @@
         rotation = (rotation + angle) % 360;
         rotated.style.transform = `rotate(${rotation}deg)`;
     }
+
+    function toggleImage(){
+        const image1 = document.getElementById("jax");
+        const image2 = document.getElementById("jeff");
+
+        image1.classList.toggle('hidden');
+        image2.classList.toggle('hidden');
+    }
