fe457e26dcd58637b11bbee7312899808ea89e5e
Jerezard <jeremys-1999@hotmail.com>
2024-03-21 17:24:29 +0100

    Page for searching cars

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
 
-    const rotated = document.getElementById('phreak');
-    let rotation = 0;
-    const angle = 90;
-
-    function rotateImage(){
-        rotation = (rotation + angle) % 360;
-        rotated.style.transform = `rotate(${rotation}deg)`;
-    }
-
-    function toggleImage(){
-        const image1 = document.getElementById("jax");
-        const image2 = document.getElementById("jeff");
-
-        image1.classList.toggle('hidden');
-        image2.classList.toggle('hidden');
-    }
+let carBrand = ["Mercedes", "BMW", "Toyota", "Honda", "Lexus"]
